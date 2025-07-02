b032a66ce8498f24dab7c7006294aab8938f489c
knopgm <knop.gm@gmail.com>
2025-07-02 18:06:38 +0200

    feat(LottoMachine): assign ranks depending on winning numbers

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -2,21 +2,21 @@ package lotto
 
 class LottoMachine(val userAmount: Int) {
 
-    private fun calculateTickets() : Int{
+    private fun calculateTickets(): Int {
         val numberOfTickets = userAmount / 1000
         return numberOfTickets
     }
 
-    private fun generateNumbers() : List<Int> {
+    private fun generateNumbers(): List<Int> {
         val randomNumbers = (1..45).shuffled().take(6).sorted()
         return randomNumbers
     }
 
-    fun createTickets() : List<Lotto> {
+    fun createTickets(): List<Lotto> {
         val lottos = mutableListOf<Lotto>()
         val amountOfTickets = calculateTickets()
         var count = 0
-        while (count < amountOfTickets ) {
+        while (count < amountOfTickets) {
             val lotto = Lotto(generateNumbers())
             lottos.add(lotto)
             count++
@@ -24,7 +24,44 @@ class LottoMachine(val userAmount: Int) {
         return lottos
     }
 
-    fun compareTickets(lottos : List<Lotto>, winningNumber: List<String>, bonusNumber: Int){
+    fun compareTicketToWinningNumbers(lotto: Lotto, winningNumbers: List<String>): Int {
+        var countMatches = 0
+        winningNumbers.forEach { number ->
+            if (number.toInt() in lotto.getNumbers()) {
+                countMatches++
+            }
+        }
+        return countMatches
+    }
 
+    fun compareTicketToBonusNumber(lotto: Lotto, bonusNumber: Int): Boolean {
+        return bonusNumber in lotto.getNumbers()
+    }
+
+    fun createMap(): MutableMap<Rank, Int> {
+        val prizeCounter = mutableMapOf<Rank, Int>()
+        for (rank in Rank.values()){
+            prizeCounter[rank] = 0
+        }
+        return prizeCounter
+    }
+    fun compareTickets(lottos: List<Lotto>, winningNumber: List<String>, bonusNumber: Int) {
+
+        val prizeCounter = createMap()
+        prizeCounter.forEach { prize -> println("${prize.key} : ${prize.value}") }
+
+        println(prizeCounter.getValue(Rank.SECOND))
+        lottos.forEach { ticket ->
+            var hasBonus = false
+
+            val matches = compareTicketToWinningNumbers(ticket, winningNumber)
+            if (matches == 5) {
+                hasBonus = compareTicketToBonusNumber(ticket, bonusNumber)
+            }
+            val chosen = Rank.valueOf(matches, hasBonus)
+            prizeCounter[chosen] = prizeCounter.getValue(chosen) + 1
+
+        }
+        prizeCounter.forEach { prize -> println("${prize.key} : ${prize.value}") }
     }
 }
