497a4cb9ddd5c125f5c2ebc5de0fc5ba5970e509
Sanghun Park <sanghun.park@deliveryhero.com>
2025-07-03 17:26:26 +0200

    feat(Application): Add 'retryable()' function to prevent unexpected exit of program during the user interaction

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,14 +1,14 @@
 package lotto
 
 fun main() {
-    val amountOfMoney = InputView.getPurchaseAmount()
+    val amountOfMoney = retryable { InputView.getPurchaseAmount() }
     val printer = LottoPrinter(amountOfMoney)
 
     ResultView.displayNumberOfTickets(printer.amountOfTicket)
     ResultView.displayTickets(printer.bundleOfLottoTicket)
 
-    val lastWeekWinningNumbers = InputView.getLastWeekWinningNumbers()
-    val bonusNumber = InputView.getBonusNumber()
+    val lastWeekWinningNumbers = retryable { InputView.getLastWeekWinningNumbers() }
+    val bonusNumber = retryable { InputView.getBonusNumber() }
     println()
 
     val machine = LottoMachine(amountOfMoney, lastWeekWinningNumbers, bonusNumber)
@@ -21,4 +21,14 @@ fun main() {
 
     ResultView.displayWinningStatistics(machine.resultTable)
     ResultView.displayReturnRate(winStats.returnRate)
+}
+
+private fun <T> retryable(inputMethod: () -> T): T {
+    while (true) {
+        try {
+            return inputMethod()
+        } catch (err: IllegalArgumentException) {
+            println("${err.message}")
+        }
+    }
 }
