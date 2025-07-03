c755e03acf990866fd11a47d187e824cb437091b
Sanghun Park <sanghun.park@deliveryhero.com>
2025-07-03 15:36:57 +0200

    feat(LottoMachine): add 'writeResultTable()' and  'getRankOfTicket()'
    -Add forEach loop to get resultTable
    -Add return Rank.of to get the rank

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -7,6 +7,7 @@ class LottoMachine(
 ) {
     val amountOfTicket: Int = amountOfMoney / LottoTicket.COST_OF_TICKET
     val bundleOfLottoTicket: List<LottoTicket> = generateLottoTickets(amountOfTicket)
+    val resultTable: MutableList<Int> = MutableList(6) { 0 }
 
     fun generateLottoTickets(amountOfTicket: Int): List<LottoTicket> {
         val tempList = mutableListOf<LottoTicket>()
@@ -16,5 +17,24 @@ class LottoMachine(
             tempList.add(ticket)
         }
         return tempList
+    }
+
+    fun writeResultTable() {
+        bundleOfLottoTicket.forEach { ticket ->
+            when (getRankOfTicket(ticket)) {
+                Rank.FIRST -> resultTable[1]++
+                Rank.SECOND -> resultTable[2]++
+                Rank.THIRD -> resultTable[3]++
+                Rank.FOURTH -> resultTable[4]++
+                Rank.FIFTH -> resultTable[5]++
+                Rank.MISS -> resultTable[0]++
+            }
+        }
+    }
+
+    private fun getRankOfTicket(ticket: LottoTicket): Rank {
+        val count = ticket.numbers.count { lastWeekWinningNumbers.contains(it) }
+        val hasBonusNumber = ticket.numbers.contains(bonusNumber)
+        return Rank.of(count, hasBonusNumber)
     }
 }
