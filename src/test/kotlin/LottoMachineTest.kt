e9d9193e32c8b7a41e346ca225a638bdb9509751
knopgm <knop.gm@gmail.com>
2025-07-03 16:51:29 +0200

    test(LottoMachine): test return rate

--- src/test/kotlin/LottoMachineTest.kt
+++ src/test/kotlin/LottoMachineTest.kt
@@ -6,22 +6,30 @@ import kotlin.test.DefaultAsserter.assertEquals
 class LottoMachineTest {
     @Test
     fun `should return right amount of tickets`() {
-        val lottoMachine = LottoMachine(5000)
-        assertEquals("Amount isn't a multiple of 1000", lottoMachine.createTickets().count(), 5)
+        val lottoMachine = LottoMachine()
+        assertEquals("Amount isn't a multiple of 1000", lottoMachine.createTickets(5000).count(), 5)
     }
 
     @Test
     fun `ticket should contain 6 numbers`() {
-        val lottoMachine = LottoMachine(1000)
-        val tickets = lottoMachine.createTickets()
+        val lottoMachine = LottoMachine()
+        val tickets = lottoMachine.createTickets(1000)
         val numbersInTicket = tickets[0].getNumbers()
-
         assertEquals("Ticket numbers aren't 6", numbersInTicket.count(), 6)
     }
 
     @Test
     fun `amount of tickets were generated`() {
-        val lottoMachine = LottoMachine(4000)
-        assertEquals("Amount of tickets is different then expected", lottoMachine.createTickets().count(), 4)
+        val lottoMachine = LottoMachine()
+        assertEquals("Amount of tickets is different then expected", lottoMachine.createTickets(4000).count(), 4)
+    }
+
+    @Test
+    fun `correct amount of rate is calculated`() {
+        val lottoMachine = LottoMachine()
+        val results = mutableMapOf<Rank, Int>()
+        results[Rank.FIFTH] = 1
+        val amount = 3000
+        assertEquals("Rate result is different then expected", lottoMachine.calculateReturnRate(results, amount), 1.6666666666666667)
     }
 }
