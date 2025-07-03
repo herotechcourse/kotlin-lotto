aed5b631583425674bb820cb9fd3fa7c957c3d32
knopgm <knop.gm@gmail.com>
2025-07-03 16:49:47 +0200

    fix(Lotto): add init conditions

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,9 +3,25 @@ package lotto
 class Lotto(val lottoNumbers: List<Int>) {
     init {
         require(lottoNumbers.count() == 6)
+        require(isInRange(lottoNumbers))
+        require(isDuplicated(lottoNumbers))
     }
 
     fun getNumbers(): List<Int> {
         return lottoNumbers
+    }
+
+    private fun isInRange(lottoNumbers: List<Int>): Boolean {
+        var isInRange = true
+        lottoNumbers.forEach { number ->
+            if (number !in 1..45) {
+                isInRange = false
+            }
+        }
+        return isInRange
+    }
+
+    private fun isDuplicated(lottoNumbers: List<Int>): Boolean {
+        return lottoNumbers.count() == lottoNumbers.toSet().count()
     }
 }
