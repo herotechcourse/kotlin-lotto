e65668fd0f82bcd6af9037b62cec6b8a9b6c452c
Sanghun Park <po4865@gmail.com>
2025-07-04 19:36:19 +0200

    Update src/main/kotlin/lotto/LottoPrinter.kt
    - apply suggestion from reviewer @da-nyee
    
    Co-authored-by: Dani <leedaeun.dev@gmail.com>

--- src/main/kotlin/lotto/LottoPrinter.kt
+++ src/main/kotlin/lotto/LottoPrinter.kt
@@ -6,8 +6,7 @@ class LottoPrinter(val amountOfMoney: Int) {
 
     fun generateLottoTickets(amountOfTicket: Int): List<LottoTicket> {
         val tempList = mutableListOf<LottoTicket>()
-        (1..amountOfTicket).forEach {
-            it
+        repeat((1..amountOfTicket).count()) {
             val ticket = LottoTicket(RandomNumberGenerator.generateNumber())
             tempList.add(ticket)
         }
