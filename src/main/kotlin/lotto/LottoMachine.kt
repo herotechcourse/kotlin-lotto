89e40d5a79175757c6dee661cd33091642517f4d
pebencze <petra.bencze@deliveryhero.com>
2025-07-04 15:58:59 +0200

    refactor(LottoMachine): omit Companion

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -1,7 +1,7 @@
 package lotto
 
 class LottoMachine(val purchaseAmount: Int, val tickets: MutableList<Lotto> = emptyList<Lotto>().toMutableList()) {
-    private val numberList = (Lotto.Companion.MIN..Lotto.Companion.MAX)
+    private val numberList = (Lotto.MIN..Lotto.MAX)
     private var change = 0
     private var ticketCount = 0
 
