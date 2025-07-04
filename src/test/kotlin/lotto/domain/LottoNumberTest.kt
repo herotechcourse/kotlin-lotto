0afa1d67e25e25cc7c63d13e8a7dcfd7c7d005b0
Mina Kim <mina.kim@deliveryhero.com>
2025-07-04 15:39:50 +0200

    test(domain): modulize LottoNumberTest.kt tests

--- src/test/kotlin/lotto/domain/LottoNumberTest.kt
+++ src/test/kotlin/lotto/domain/LottoNumberTest.kt
@@ -3,28 +3,25 @@ package lotto.domain
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class LottoNumberTest {
 
-    @Test
-    fun `throw if lotto number not in range, too small`() {
+    @ValueSource(ints = [LottoNumber.MIN_RANGE_NUMBER - 1, LottoNumber.MIN_RANGE_NUMBER - 2,
+        LottoNumber.MAX_RANGE_NUMBER + 1, LottoNumber.MAX_RANGE_NUMBER + 2])
+    @ParameterizedTest
+    fun `throw if lotto number not in range`(value: Int) {
         assertThrows<IllegalArgumentException> {
-            val numberZero = LottoNumber.from(LottoNumber.MIN_RANGE_NUMBER - 1)
+            val number = LottoNumber.from(value)
         }
     }
 
-    @Test
-    fun `throw if lotto number not in range, too big`() {
-        assertThrows<IllegalArgumentException> {
-            val numberZero = LottoNumber.from(LottoNumber.MAX_RANGE_NUMBER + 1)
-        }
-    }
-
-    @Test
-    fun `do not throw if lotto number in range`() {
+    @ValueSource(ints = [LottoNumber.MIN_RANGE_NUMBER, LottoNumber.MAX_RANGE_NUMBER])
+    @ParameterizedTest
+    fun `do not throw if lotto number in range`(value: Int) {
         assertDoesNotThrow {
-            val numberOne = LottoNumber.from(LottoNumber.MIN_RANGE_NUMBER)
-            val numberTwo = LottoNumber.from(LottoNumber.MAX_RANGE_NUMBER)
+            val number = LottoNumber.from(value)
         }
     }
 }
