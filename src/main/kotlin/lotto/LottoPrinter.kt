d50ac27232b13096e34d407e7b4e919612fbce1d
Sanghun Park <sanghun.park@deliveryhero.com>
2025-07-04 22:52:43 +0200

    refactor(LottoPrinter): make 'generateLottoTickets()' to private
    - rename variable name 'tempList' to 'tickets' for better readability.

--- src/main/kotlin/lotto/LottoPrinter.kt
+++ src/main/kotlin/lotto/LottoPrinter.kt
@@ -1,15 +1,15 @@
 package lotto
 
-class LottoPrinter(val amountOfMoney: Int) {
+class LottoPrinter(amountOfMoney: Int) {
     val amountOfTicket: Int = amountOfMoney / LottoTicket.COST_OF_TICKET
     val bundleOfLottoTicket: List<LottoTicket> = generateLottoTickets(amountOfTicket)
 
-    fun generateLottoTickets(amountOfTicket: Int): List<LottoTicket> {
-        val tempList = mutableListOf<LottoTicket>()
+    private fun generateLottoTickets(amountOfTicket: Int): List<LottoTicket> {
+        val tickets = mutableListOf<LottoTicket>()
         repeat((1..amountOfTicket).count()) {
             val ticket = LottoTicket(RandomNumberGenerator.generateNumber())
-            tempList.add(ticket)
+            tickets.add(ticket)
         }
-        return tempList
+        return tickets
     }
 }
