14631c67f5e97c63b4bc4a6df6c7bc10f6c71c6a
Sanghun Park <sanghun.park@deliveryhero.com>
2025-07-04 23:04:00 +0200

    refactor(InputView): add custom error message for invalid user input
    - add constant value inside a new inner object to use error message for invalid user input on numbers for Lotto number.

--- src/main/kotlin/lotto/InputView.kt
+++ src/main/kotlin/lotto/InputView.kt
@@ -7,7 +7,7 @@ object InputView {
         println("Please enter the purchase amount.")
         val input = readln()
         val userInput = input.toIntOrNull() ?: throw IllegalArgumentException("This is not a number.")
-        require(userInput % 1000 == 0)
+        require(userInput % 1000 == 0) { "You must provide a valid amount of purchase." }
         return userInput
     }
 
@@ -18,8 +18,8 @@ object InputView {
         val usedInput = input.split(",") // -> ["1", " 2", " 3", " 4", " 5", " 6"]
         val userInput = usedInput.map { it.toIntOrNull() ?: throw IllegalArgumentException("This is not a number.") }
         userInput.forEach {
-            require(it > 0)
-            require(it < 46)
+            require(it >= LottoTicket.MIN_LOTTO_NUMBER) { Message.ERROR_MESSAGE_FOR_LOTTO_NUMBER }
+            require(it <= LottoTicket.MAX_LOTTO_NUMBER) { Message.ERROR_MESSAGE_FOR_LOTTO_NUMBER }
         }
         val finalInput = userInput.distinct()
         require(finalInput.size == 6) { "Invalid Input." }
@@ -30,8 +30,13 @@ object InputView {
         println("Please enter the bonus number.")
         val input = readln()
         val userInput = input.toIntOrNull() ?: throw IllegalArgumentException("This is not a number.")
-        require(userInput > 0)
-        require(userInput < 46)
+        require(userInput >= LottoTicket.MIN_LOTTO_NUMBER) { Message.ERROR_MESSAGE_FOR_LOTTO_NUMBER }
+        require(userInput <= LottoTicket.MAX_LOTTO_NUMBER) { Message.ERROR_MESSAGE_FOR_LOTTO_NUMBER }
         return userInput
+    }
+
+    object Message {
+        const val ERROR_MESSAGE_FOR_LOTTO_NUMBER =
+            "You must provide a number between ${LottoTicket.MIN_LOTTO_NUMBER} and ${LottoTicket.MAX_LOTTO_NUMBER}."
     }
 }
