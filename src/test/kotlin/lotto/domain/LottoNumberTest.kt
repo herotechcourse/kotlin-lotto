d4d50aa0884b9eb58c6857cc4dff42f25ed7e928
Mina Kim <mina.kim@deliveryhero.com>
2025-07-04 15:34:33 +0200

    test(domain): add unit tests for value bounds (too small, too big, in range)

--- src/test/kotlin/lotto/domain/LottoNumberTest.kt
+++ src/test/kotlin/lotto/domain/LottoNumberTest.kt
@@ -1,14 +1,30 @@
 package lotto.domain
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class LottoNumberTest {
 
     @Test
-    fun `throw if lotto number not in range`() {
+    fun `throw if lotto number not in range, too small`() {
         assertThrows<IllegalArgumentException> {
-            val numberZero = LottoNumber.from(0)
+            val numberZero = LottoNumber.from(LottoNumber.MIN_RANGE_NUMBER - 1)
+        }
+    }
+
+    @Test
+    fun `throw if lotto number not in range, too big`() {
+        assertThrows<IllegalArgumentException> {
+            val numberZero = LottoNumber.from(LottoNumber.MAX_RANGE_NUMBER + 1)
+        }
+    }
+
+    @Test
+    fun `do not throw if lotto number in range`() {
+        assertDoesNotThrow {
+            val numberOne = LottoNumber.from(LottoNumber.MIN_RANGE_NUMBER)
+            val numberTwo = LottoNumber.from(LottoNumber.MAX_RANGE_NUMBER)
         }
     }
 }
