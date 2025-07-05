3affe24e20e921c109b90374da48f774954bab0f
Mina Kim <mina.kim@deliveryhero.com>
2025-07-05 20:02:49 +0200

    test(domain): add tests to verify sufficient and insufficient numbers as parameter

--- src/test/kotlin/lotto/domain/LottoTicketTest.kt
+++ src/test/kotlin/lotto/domain/LottoTicketTest.kt
@@ -1,13 +1,45 @@
 package lotto.domain
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class LottoTicketTest {
     @Test
-    fun `should throw when lotto ticket has insufficient numbers`() {
+    fun `throw when lotto ticket has insufficient numbers`() {
         assertThrows<IllegalArgumentException> {
             LottoTicket(hashSetOf())
         }
     }
+
+    @Test
+    fun `does not throw when lotto ticket has sufficient numbers`() {
+        assertDoesNotThrow {
+            val setOfLottoNumber = hashSetOf(
+                LottoNumber.from(1),
+                LottoNumber.from(2),
+                LottoNumber.from(3),
+                LottoNumber.from(4),
+                LottoNumber.from(5),
+                LottoNumber.from(6)
+            )
+            LottoTicket(setOfLottoNumber)
+        }
+    }
+
+    @Test
+    fun `throw when lotto ticket has sufficient numbers, but numbers not in range`() {
+        assertThrows<IllegalArgumentException> {
+            val setOfLottoNumber = hashSetOf(
+                LottoNumber.from(0),
+                LottoNumber.from(1),
+                LottoNumber.from(2),
+                LottoNumber.from(3),
+                LottoNumber.from(4),
+                LottoNumber.from(5),
+            )
+            LottoTicket(setOfLottoNumber)
+        }
+    }
+
 }
