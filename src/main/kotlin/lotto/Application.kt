8c29a231ac7811562d1cc55371d5c227a9f59835
knopgm <knop.gm@gmail.com>
2025-07-05 18:13:24 +0200

    refactor(Application): remove unnecessary comments

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,18 +5,15 @@ fun main() {
     val lottoMachine = LottoMachine()
     val resultView = ResultView()
 
-    // Create a ticket from given amount
     val userAmount = inputView.takePurchaseInput()
     val tickets = lottoMachine.createTickets(userAmount)
     resultView.printLottos(tickets)
 
-    // Compare winning tickets from given winning numbers
     val winningNumbers = inputView.takeWinningNumbers()
     val bonusNumber = inputView.takeBonusNumber(winningNumbers)
     val results = lottoMachine.compareTickets(tickets, winningNumbers, bonusNumber)
     resultView.printResult(results)
 
-    // Calculate profit rate
     val rate = lottoMachine.calculateReturnRate(results, userAmount)
     resultView.printReturnRate(rate)
 }
