05b8ff8992360b254c03fab5673cd6df136fc857
Mina Kim <mina.kim@deliveryhero.com>
2025-07-05 23:47:52 +0200

    test: remove unnecessary dependency on PendingTicket/s and revert to raw data format

--- src/test/kotlin/lotto/services/TicketIssuerTest.kt
+++ src/test/kotlin/lotto/services/TicketIssuerTest.kt
@@ -1,17 +1,43 @@
 package lotto.services
 
+import lotto.domain.LottoTicket
 import lotto.dto.IssuedTickets
-import lotto.dto.PendingTicket
-import lotto.dto.PendingTickets
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
 
 class TicketIssuerTest {
     @Test
     fun `throw if can not issue ticket`() {
         assertThrows<IllegalArgumentException> {
-            val pendingTickets = PendingTickets(listOf(PendingTicket(setOf(0, 1, 2, 3, 4, 5, 6))))
-            val issuedTickets: IssuedTickets = TicketIssuer.with(pendingTickets)
+            val requests = listOf(
+                    setOf(0, 1, 2, 3, 4, 5, 6),
+                    setOf(46, 45, 44, 43, 42, 41)
+                )
+            val issuedTickets = TicketIssuer.with(requests)
         }
+    }
+
+    @Test
+    fun `dose not throw if issue ticket`() {
+        assertDoesNotThrow {
+            val requests = listOf(
+                    setOf(1, 2, 3, 4, 5, 6),
+                    setOf(45, 44, 43, 42, 41, 40)
+                )
+            val issuedTickets = TicketIssuer.with(requests)
+        }
+    }
+
+    @Test
+    fun `should return correct string`() {
+        val issued = IssuedTickets(
+            listOf(
+                LottoTicket.from(setOf(1, 2, 3, 4, 5, 6))
+            )
+        )
+
+        assertThat(issued.toString()).isEqualTo("1, 2, 3, 4, 5, 6")
     }
 }
