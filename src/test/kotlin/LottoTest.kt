b6b09119487253b3f36290607ead0daaac52fbed
knopgm <knop.gm@gmail.com>
2025-07-06 16:52:03 +0200

    test(LottoTest): add Lotto tests to assert correct output of calculateRank function

--- src/test/kotlin/LottoTest.kt
+++ src/test/kotlin/LottoTest.kt
@@ -1,5 +1,6 @@
 package lotto
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -26,5 +27,77 @@ class LottoTest {
         assertThrows<IllegalArgumentException> {
             Lotto(lottoNumbers)
         }
+    }
+
+    @Test
+    fun `6 matching numbers without bonus returns FIRST prize`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningNumbers = listOf("1", "2", "3", "4", "5", "6")
+        val bonusNumber = 8
+        assertEquals(
+            Rank.FIRST,
+            lotto.calculateRank(winningNumbers, bonusNumber),
+            "Rate result is different than expected",
+        )
+    }
+
+    @Test
+    fun `5 matching numbers with bonus returns SECOND prize`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 7))
+        val winningNumbers = listOf("1", "2", "3", "4", "5", "6")
+        val bonusNumber = 7
+        assertEquals(
+            Rank.SECOND,
+            lotto.calculateRank(winningNumbers, bonusNumber),
+            "Rate result is different than expected",
+        )
+    }
+
+    @Test
+    fun `5 matching numbers without bonus returns THIRD prize`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 5, 7))
+        val winningNumbers = listOf("1", "2", "3", "4", "5", "6")
+        val bonusNumber = 6
+        assertEquals(
+            Rank.THIRD,
+            lotto.calculateRank(winningNumbers, bonusNumber),
+            "Rate result is different than expected",
+        )
+    }
+
+    @Test
+    fun `4 matching numbers without bonus returns FOUR prize`() {
+        val lotto = Lotto(listOf(1, 2, 3, 4, 44, 45))
+        val winningNumbers = listOf("1", "2", "3", "4", "5", "6")
+        val bonusNumber = 8
+        assertEquals(
+            Rank.FOURTH,
+            lotto.calculateRank(winningNumbers, bonusNumber),
+            "Rate result is different than expected",
+        )
+    }
+
+    @Test
+    fun `3 matching numbers without bonus returns FIFTH prize`() {
+        val lotto = Lotto(listOf(1, 2, 3, 43, 44, 45))
+        val winningNumbers = listOf("1", "2", "3", "4", "5", "6")
+        val bonusNumber = 6
+        assertEquals(
+            Rank.FIFTH,
+            lotto.calculateRank(winningNumbers, bonusNumber),
+            "Rate result is different than expected",
+        )
+    }
+
+    @Test
+    fun `less than 3 matching numbers without bonus returns MISS`() {
+        val lotto = Lotto(listOf(1, 2, 30, 40, 41, 42))
+        val winningNumbers = listOf("1", "2", "3", "4", "5", "6")
+        val bonusNumber = 40
+        assertEquals(
+            Rank.MISS,
+            lotto.calculateRank(winningNumbers, bonusNumber),
+            "Rate result is different than expected",
+        )
     }
 }
