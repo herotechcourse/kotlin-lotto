fc972ee876161760f65c55244ca8f39aeff2a0f9
knopgm <knop.gm@gmail.com>
2025-07-06 11:18:14 +0200

    refactor(ResultsView): apply multiline console and pattern to the messages at the ResultsView class

--- src/main/kotlin/lotto/ResultView.kt
+++ src/main/kotlin/lotto/ResultView.kt
@@ -10,23 +10,33 @@ class ResultView() {
     }
 
     fun printResult(results: MutableMap<Rank, Int>) {
-        println()
-        println(PRESENT_WINNING)
-        println(SEPARATOR)
-        println("3 Matches (5,000 KRW) - ${results.getValue(Rank.FIFTH)} tickets")
-        println("4 Matches (50,000 KRW) - ${results.getValue(Rank.FOURTH)} tickets")
-        println("5 Matches (1,500,000 KRW) - ${results.getValue(Rank.THIRD)} tickets")
-        println("5 Matches + Bonus Ball (30,000,000 KRW) - ${results.getValue(Rank.SECOND)} tickets")
-        println("6 Matches (2,000,000,000 KRW) - ${results.getValue(Rank.FIRST)} tickets")
+        println(
+            """
+            
+            ${PRESENT_WINNING}
+            $SEPARATOR
+            ${THREE_MATCHES_RESULT.format(results.getValue(Rank.FIFTH))}
+            ${FOUR_MATCHES_RESULT.format(results.getValue(Rank.FOURTH))}
+            ${FIVE_MATCHES_RESULT.format(results.getValue(Rank.THIRD))}
+            ${FIVE_MATCHES_RESULT_WITH_BONUS.format(results.getValue(Rank.SECOND))}
+            ${SIX_MATCHES_RESULT.format(results.getValue(Rank.FIRST))}
+            """.trimIndent(),
+        )
     }
 
     fun printReturnRate(rate: Double) {
         val formattedRate = String.format("%.1f", rate).replace(',', '.')
-        println("Total return rate is $formattedRate")
+        println(PRESENT_RATE.format(formattedRate))
     }
 
     companion object Messages {
         const val PRESENT_WINNING = "Winning Statistics"
         const val SEPARATOR = "------------------"
+        const val THREE_MATCHES_RESULT = "3 Matches (5,000 KRW) - %s tickets"
+        const val FOUR_MATCHES_RESULT = "4 Matches (50,000 KRW) - %s tickets"
+        const val FIVE_MATCHES_RESULT = "5 Matches (1,500,000 KRW) - %s tickets"
+        const val FIVE_MATCHES_RESULT_WITH_BONUS = "5 Matches + Bonus Ball (30,000,000 KRW) - %s tickets"
+        const val SIX_MATCHES_RESULT = "6 Matches (2,000,000,000 KRW) - %s tickets"
+        const val PRESENT_RATE = "Total return rate is %s"
     }
 }
