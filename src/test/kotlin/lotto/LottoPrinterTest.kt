2b9155d83f8e50ff88b6c8068f69fb1ad62b9d72
Sanghun Park <sanghun.park@deliveryhero.com>
2025-07-07 17:27:05 +0200

    chore(LottoPrinterTest): add parameterized test for generation of auto-numbered tickets

---
+++ src/test/kotlin/lotto/LottoPrinterTest.kt
@@ -0,0 +1,15 @@
+package lotto
+
+import lotto.model.LottoPrinter
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class LottoPrinterTest {
+    @ParameterizedTest(name = "is LottoPrinter generate {0} number of lotto tickets")
+    @ValueSource(ints = [1, 2, 3, 14, 25, 35])
+    fun `LottoPrinter generate auto-numbered lotto tickets based on the number of tickets as a input`(candidate: Int) {
+        val printer = LottoPrinter(candidate)
+        assertEquals(candidate, printer.bundleOfLottoTicket.size)
+    }
+}
