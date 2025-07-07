4038587ed7c19bcefecbf16a062ee4a9b32bf14a
saragrosser <sgrosser00@gmail.com>
2025-07-07 15:51:06 +0200

    refactor(printer): rewrite ticket generation in functional style
    -Used List Kotlin's functional List constructor

--- src/main/kotlin/lotto/LottoPrinter.kt
+++ src/main/kotlin/lotto/LottoPrinter.kt
@@ -5,12 +5,8 @@ class LottoPrinter(val amountOfMoney: Int) {
     val bundleOfLottoTicket: List<LottoTicket> = generateLottoTickets(amountOfTicket)
 
     fun generateLottoTickets(amountOfTicket: Int): List<LottoTicket> {
-        val tempList = mutableListOf<LottoTicket>()
-        (1..amountOfTicket).forEach {
-            it
-            val ticket = LottoTicket(RandomNumberGenerator.generateNumber())
-            tempList.add(ticket)
+        return List(amountOfTicket) {
+            LottoTicket(RandomNumberGenerator.generateNumber())
         }
-        return tempList
     }
 }
