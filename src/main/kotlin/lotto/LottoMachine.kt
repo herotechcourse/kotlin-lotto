1bfb6dd31a86c82998c878636ac40986efaf0d37
saragrosser <sgrosser00@gmail.com>
2025-07-07 23:41:02 +0200

    refactor(LottoMachine): remove filterTicket and writeResultTable to eliminate internal state

--- src/main/kotlin/lotto/LottoMachine.kt
+++ src/main/kotlin/lotto/LottoMachine.kt
@@ -1,20 +1,15 @@
 package lotto
 
 class LottoMachine(
-    val amountOfMoney: Int,
-    val lastWeekWinningNumbers: List<Int>,
-    val bonusNumber: Int,
+    private val lastWeekWinningNumbers: List<Int>,
+    private val bonusNumber: Int,
 ) {
-    val amountOfTicket: Int = amountOfMoney / LottoTicket.COST_OF_TICKET
-    lateinit var bundleOfLottoTicket: List<LottoTicket>
-    val resultTable: MutableMap<Rank, Int> = mutableMapOf()
-
-    fun writeResultTable() {
-        bundleOfLottoTicket.forEach { filterTicket(it) }
-    }
-
-    fun filterTicket(ticket: LottoTicket) {
-        val rank = Rank.ofTicket(ticket, lastWeekWinningNumbers, bonusNumber)
-        resultTable[rank] = resultTable.getOrDefault(rank, 0) + 1
+    fun createResultTable(tickets: List<LottoTicket>): Map<Rank, Int> {
+        val result = mutableMapOf<Rank, Int>()
+        tickets.forEach { ticket ->
+            val rank = Rank.ofTicket(ticket, lastWeekWinningNumbers, bonusNumber)
+            result[rank] = result.getOrDefault(rank, 0) + 1
+        }
+        return result
     }
 }
