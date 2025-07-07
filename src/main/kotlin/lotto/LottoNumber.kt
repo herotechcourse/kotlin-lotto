970651fdd62ca31f807f854d0a6b48756f2c188e
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-07-07 16:52:03 +0200

    refactor(result): override compareTo and toString in LottoNumber

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -1,9 +1,17 @@
 package lotto
 
-class LottoNumber private constructor(private val value: Int) {
+class LottoNumber private constructor(private val value: Int) : Comparable<LottoNumber> {
+    override fun compareTo(other: LottoNumber): Int {
+        return value - other.value
+    }
+
+    override fun toString(): String {
+        return value.toString()
+    }
+
     companion object {
-        private const val MINIMUM_NUMBER = 1
-        private const val MAXIMUM_NUMBER = 45
+        const val MINIMUM_NUMBER = 1
+        const val MAXIMUM_NUMBER = 45
         private const val ERROR_MSG = "Value is not in range from $MINIMUM_NUMBER to $MAXIMUM_NUMBER"
         private val NUMBERS: Map<Int, LottoNumber> =
             (MINIMUM_NUMBER..MAXIMUM_NUMBER).associateWith(::LottoNumber)
