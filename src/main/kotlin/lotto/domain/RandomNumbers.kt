bd24c357ad492b984819727630a0a12e13bfeb13
Mina Kim <mina.kim@deliveryhero.com>
2025-07-07 16:42:06 +0200

    feat(domain): implement generate randomNumbers logic

---
+++ src/main/kotlin/lotto/domain/RandomNumbers.kt
@@ -0,0 +1,11 @@
+package lotto.domain
+
+object RandomNumbers {
+    fun get(): Set<Int> {
+        return (LottoNumber.MIN_RANGE_NUMBER..LottoNumber.MAX_RANGE_NUMBER)
+            .shuffled()
+            .take(LottoTicket.SUFFICIENT_SIZE)
+            .sorted()
+            .toCollection(linkedSetOf())
+    }
+}
