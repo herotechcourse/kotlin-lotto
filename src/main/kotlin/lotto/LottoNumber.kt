39509b73a7546d428e51e9cea9c9ca94c38d5b2d
pebencze <petra.bencze@deliveryhero.com>
2025-07-07 14:20:51 +0200

    refactor(LottoNumber): change visibility

--- src/main/kotlin/lotto/LottoNumber.kt
+++ src/main/kotlin/lotto/LottoNumber.kt
@@ -2,8 +2,8 @@ package lotto
 
 class LottoNumber private constructor(private val number: Int) {
     companion object {
-        private const val MIN = 1
-        private const val MAX = 45
+        const val MIN = 1
+        const val MAX = 45
         private val NUMBERS: Map<Int, LottoNumber>
             = (MIN..MAX).associateWith(::LottoNumber)
 
