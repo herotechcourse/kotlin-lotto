aa01b9527953922c848a4e5afeeb26e4f4e4aa41
Mina Kim <mina.kim@deliveryhero.com>
2025-07-07 11:48:51 +0200

    feat(controller): Add new helper methods get/ask
    - ask: Prompt the user to ask a question and get input.
    - get: Similar to getter

--- src/main/kotlin/lotto/controller/Controller.kt
+++ src/main/kotlin/lotto/controller/Controller.kt
@@ -1,36 +1,62 @@
 package lotto.controller
 
+
+import lotto.domain.LottoTicket
+import lotto.services.TicketIssuer
 import lotto.view.InputView
 import lotto.view.OutputView
 
 object Controller {
     fun run() {
         // get amount
-        val amount = getAmount()
+        val amount = askAmount()
+        val totalTicketsNumber = getTotalTicketsNumber(amount)
+
         // get manual tickets
-        val numberOfManual = getNumberOfManual(amount)
+        val manualTicketsNumber = askManualTicketsNumbers(amount)
+
         // get the numbers for manual tickets
-        val manualNumbersRaw = getManualNumbers(numberOfManual)
+        val manualNumbersRaw = askManualNumbersRaw(manualTicketsNumber)
+        val issuedManualTickets = TicketIssuer.with(manualNumbersRaw)
+
+        // get random tickets
+        val randomTicketsNumber = getRandomTicketsNumber(totalTicketsNumber, manualTicketsNumber)
+        val randomNumbersRaw = getRandomNumbersRaw(randomTicketsNumber)
+        val issuedRandomTickets = TicketIssuer.with(randomNumbersRaw)
+
         // show a purchased result
+        OutputView.Result.purchase(issuedManualTickets, issuedRandomTickets)
+
         // get winning main numbers
         // get bonus number
         // show winning statistics
     }
 
-    private fun getAmount(): Int {
+    private fun getTotalTicketsNumber(amount: Int) = amount / LottoTicket.PRICE_OF_TICKET
+
+    private fun getRandomTicketsNumber(total: Int, manualNumber: Int): Int {
+        require(total >= manualNumber) { "total must be >= $manualNumber" }
+        return total - manualNumber
+    }
+
+    private fun askAmount(): Int {
         OutputView.Prompt.amount()
         return InputView.readUserAmount()
     }
 
-    private fun getNumberOfManual(amount: Int): Int {
+    private fun askManualTicketsNumbers(amount: Int): Int {
         OutputView.Prompt.numberOfManual()
         return InputView.readNumberOfManual(amount)
     }
 
-    private fun getManualNumbers(numberOfManual: Int): List<Set<Int>> {
+    private fun askManualNumbersRaw(size: Int): List<Set<Int>> {
         OutputView.Prompt.manualNumbers()
-        return List(numberOfManual) {
+        return List(size) {
             InputView.readManualNumbers()
         }
+    }
+
+    private fun getRandomNumbersRaw(size: Int): List<Set<Int>> {
+        // TODO: implement
     }
 }
