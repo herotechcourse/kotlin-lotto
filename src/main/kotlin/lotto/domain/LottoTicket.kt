0f31dba375b62bb7591025a2b1410a17a836141b
Mina Kim <mina.kim@deliveryhero.com>
2025-07-07 14:11:05 +0200

    docs(domain): added simple docs at LottoTicket.kt

--- src/main/kotlin/lotto/domain/LottoTicket.kt
+++ src/main/kotlin/lotto/domain/LottoTicket.kt
@@ -10,6 +10,10 @@ data class LottoTicket(private val lottoNumbers: HashSet<LottoNumber>) {
         const val PRICE_OF_TICKET = 1000
         const val CURRENCY = "KRW"
 
+        /**
+         * LottoTicket does not guarantee sorted order.
+         * @param requests must be pre-sorted.
+         */
         fun from(requests: Set<Int>): LottoTicket {
             return LottoTicket(
                 requests.map {
