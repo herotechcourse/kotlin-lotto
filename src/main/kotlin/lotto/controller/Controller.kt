f41ca6356966909dce13cd7c6dcdf9050da78e3a
Mina Kim <mina.kim@deliveryhero.com>
2025-07-07 16:43:45 +0200

    feat(controller): implement new control logic base on new change
    - separate service
    - add more ask/read methods to help run()

--- src/main/kotlin/lotto/controller/Controller.kt
+++ src/main/kotlin/lotto/controller/Controller.kt
@@ -1,8 +1,13 @@
 package lotto.controller
 
 
+import lotto.domain.LottoNumber
 import lotto.domain.LottoTicket
+import lotto.domain.WinningCombination
+import lotto.services.GameResult
+import lotto.domain.RandomNumbers
 import lotto.services.TicketIssuer
+import lotto.services.TicketsEvaluator
 import lotto.view.InputView
 import lotto.view.OutputView
 
@@ -24,12 +29,28 @@ object Controller {
         val randomNumbersRaw = getRandomNumbersRaw(randomTicketsNumber)
         val issuedRandomTickets = TicketIssuer.with(randomNumbersRaw)
 
+        val totalIssuedTickets = issuedManualTickets + issuedRandomTickets
+
         // show a purchased result
         OutputView.Result.purchase(issuedManualTickets, issuedRandomTickets)
+        OutputView.Result.issuedTickets(totalIssuedTickets)
 
         // get winning main numbers
+        val winningNumbersRaw = askWinningNumbers()
+        val winningTicket = LottoTicket.from(winningNumbersRaw)
+
         // get bonus number
+        val bonusNumber = askBonusNumber(winningNumbersRaw)
+        val winningCombination = WinningCombination(winningTicket, bonusNumber)
+
+        val ticketRanks = TicketsEvaluator.runWith(totalIssuedTickets, winningCombination)
+
+        val returnRate = TicketsEvaluator.getTotalRate(ticketRanks, amount)
+        // get gameResult
+        val gameResult = GameResult(totalIssuedTickets, winningCombination, amount, ticketRanks, returnRate)
+
         // show winning statistics
+        OutputView.Result.winningStatistics(gameResult)
     }
 
     private fun getTotalTicketsNumber(amount: Int) = amount / LottoTicket.PRICE_OF_TICKET
@@ -40,23 +61,45 @@ object Controller {
     }
 
     private fun askAmount(): Int {
-        OutputView.Prompt.amount()
-        return InputView.readUserAmount()
+        return InputView.retryUntilSuccess(
+            prompt = { OutputView.Prompt.amount() },
+            read = { InputView.readUserAmount() }
+        )
     }
 
     private fun askManualTicketsNumbers(amount: Int): Int {
-        OutputView.Prompt.numberOfManual()
-        return InputView.readNumberOfManual(amount)
+        return InputView.retryUntilSuccess(
+            prompt = { OutputView.Prompt.numberOfManual() },
+            read = { InputView.readNumberOfManual(amount) }
+        )
     }
 
     private fun askManualNumbersRaw(size: Int): List<Set<Int>> {
-        OutputView.Prompt.manualNumbers()
-        return List(size) {
-            InputView.readManualNumbers()
-        }
+        return InputView.retryUntilSuccess(
+            prompt = { OutputView.Prompt.manualNumbers() },
+            read = {
+                List(size) {
+                    InputView.readManualNumbers(size)
+                }
+            }
+        )
     }
 
     private fun getRandomNumbersRaw(size: Int): List<Set<Int>> {
-        // TODO: implement
+        return List(size) { RandomNumbers.get() }
+    }
+
+    private fun askWinningNumbers(): Set<Int> {
+        return InputView.retryUntilSuccess(
+            prompt = { OutputView.Prompt.winningNumbers() },
+            read = { InputView.readManualNumbers(1) }
+        )
+    }
+
+    private fun askBonusNumber(winningInput: Set<Int>): LottoNumber {
+        return InputView.retryUntilSuccess(
+            prompt = { OutputView.Prompt.bonusNumber() },
+            read = { InputView.readBonusNumber(winningInput) }
+        )
     }
 }
