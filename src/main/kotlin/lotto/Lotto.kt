de5d84f1b2904b3f19a5df8a2aee3bbf5e785214
pebencze <petra.bencze@deliveryhero.com>
2025-07-07 12:59:19 +0200

    refactor(Lotto): use LottoNumber instead of Int

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,25 +1,20 @@
 package lotto
 
-class Lotto(val numbers: List<Int>) {
+class Lotto(val numbers: List<LottoNumber>) {
     init {
         require(numbers.size == LOTTO_SIZE) {
             "[ERROR] Lotto number size has to be $LOTTO_SIZE."
-        }
-        require(numbers.all { it in MIN..MAX }) {
-            "[ERROR] Range has to be $MIN..$MAX."
         }
         require(numbers.distinct().size == 6) {
             "[ERROR] Duplicates found."
         }
     }
 
-    fun hasBonusNumber(bonusNumber: Int): Boolean {
+    fun hasBonusNumber(bonusNumber: LottoNumber): Boolean {
         return numbers.contains(bonusNumber)
     }
 
     companion object {
-        const val MIN = 0
-        const val MAX = 45
         const val LOTTO_SIZE = 6
     }
 }
