4414d4f577f7ede613020e2a3927a43c7d72e525
pebencze <petra.bencze@deliveryhero.com>
2025-07-08 12:41:46 +0200

    refactor(Purchase): add getter and setter

--- src/main/kotlin/lotto/Purchase.kt
+++ src/main/kotlin/lotto/Purchase.kt
@@ -9,10 +9,18 @@ data class Purchase(val amount: Int) {
 
     val change = amount % TICKET_PRICE
     val ticketCount = amount / TICKET_PRICE
+    // TODO maybe put manualTicketCount into a value class?
+    var manualTicketsCount: Int = 0
+        set(value) {
+            require(value in 0..ticketCount) { "Number of manual tickets cannot exceed $ticketCount." }
+            field = value
+        }
+    val automaticTicketsCount: Int
+        get() = ticketCount - manualTicketsCount
 
     companion object {
         private const val MIN = 1_000
         private const val MAX = 20_000
-        private const val TICKET_PRICE = 1_000
+        internal const val TICKET_PRICE = 1_000
     }
 }
