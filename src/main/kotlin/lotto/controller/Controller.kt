28b4596ab53dba7a8682fdc52afcfc636cc50245
Sanghun Park <sanghun.park@deliveryhero.com>
2025-07-10 21:41:20 +0200

    refactor(Controller,TicketManager): add new class 'TicketManager' as a controller unit
    - extract ticket related parts from 'Controller' and add to new class 'TicketManager' to reduce responsibility of 'Controller'

--- src/main/kotlin/lotto/controller/Controller.kt
+++ src/main/kotlin/lotto/controller/Controller.kt
@@ -1,11 +1,7 @@
 package lotto.controller
 
 import lotto.model.LottoMachine
-import lotto.model.LottoPrinter
-import lotto.model.LottoTicket
 import lotto.model.Numbers
-import lotto.model.Tickets
-import lotto.util.RandomNumberGenerator
 import lotto.view.InputView
 import lotto.view.ResultView
 
@@ -13,46 +9,19 @@ object Controller {
     val inputView = InputView
     val resultView = ResultView
     private val amountOfMoney: Int = inputView.retryable { InputView.getPurchaseAmount() }
-    private val amountOfTicket: Int = amountOfMoney / LottoTicket.COST_OF_TICKET
-    private var amountOfManualTicket = 0
-    private val amountOfAutoTicket: Int
-        get() {
-            return amountOfTicket - amountOfManualTicket
-        }
-    private var bundleOfTicket: Tickets = Tickets(emptyList())
+    private val ticketManager = TicketManager(amountOfMoney)
     private var lastWeekWinningNumbers: List<Int> = emptyList()
     private var bonusNumber: Int = 0
 
     fun run() {
-        updateAmountOfManualTicket()
-        val manualTickets =
-            LottoPrinter.generateLottoTickets(amountOfManualTicket) {
-                inputView.retryable { inputView.getLottoNumbers() }
-            }
-        val autoTickets =
-            LottoPrinter.generateLottoTickets(amountOfAutoTicket) {
-                RandomNumberGenerator.generateNumber()
-            }
-        printLottoTickets(manualTickets, autoTickets)
+        printLottoTickets()
         readWinningNumbers()
         runLottoMachine()
     }
 
-    private fun updateAmountOfManualTicket() {
-        amountOfManualTicket = inputView.retryable { inputView.getNumberOfManualTickets(amountOfTicket) }
-        inputView.informForManualTicketNumbers()
-    }
-
-    private fun printLottoTickets(
-        manualTickets: Tickets,
-        autoTickets: Tickets,
-    ) {
-        val tickets = Tickets().asList().toMutableList()
-        tickets.addAll(manualTickets.asList())
-        tickets.addAll(autoTickets.asList())
-        bundleOfTicket = Tickets(tickets)
-        resultView.displayNumberOfTickets(amountOfManualTicket, amountOfAutoTicket)
-        resultView.displayTickets(bundleOfTicket)
+    private fun printLottoTickets() {
+        resultView.displayNumberOfTickets(ticketManager.amountOfManualTicket, ticketManager.amountOfAutoTicket)
+        resultView.displayTickets(ticketManager.tickets)
     }
 
     private fun readWinningNumbers() {
@@ -63,7 +32,7 @@ object Controller {
 
     private fun runLottoMachine() {
         val machine = LottoMachine(amountOfMoney, Numbers(lastWeekWinningNumbers), bonusNumber)
-        machine.bundleOfLottoTicket = bundleOfTicket
+        machine.bundleOfLottoTicket = ticketManager.tickets
         machine.writeResultTable()
 
         val winStats = machine.winStat
