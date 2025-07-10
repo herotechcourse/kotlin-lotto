87cf94dd5e084d3cfd5c09e5e9c2920c04a8f163
Mina Kim <mina.kim@deliveryhero.com>
2025-07-10 21:51:26 +0200

    feat: implement a new view object for exception

---
+++ src/main/kotlin/lotto/view/ExceptionView.kt
@@ -0,0 +1,12 @@
+package lotto.view
+
+object ExceptionView {
+    object Amount {
+        const val TOO_SMALL = "too small"
+        const val NOT_DIVISIBLE = "not divisible"
+    }
+
+    object LottoTicket {
+        const val NOT_SUFFICIENT_SIZE = "lotto ticket has not sufficient size"
+    }
+}
