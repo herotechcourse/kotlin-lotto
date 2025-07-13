4b279669db4ab6aabe0e9bcda3fb5e24b3fe07e1
Mariia Kulikova <mariia.kulikova@deliveryhere.com>
2025-07-13 13:51:12 +0200

    test(lotto): update test code, pass list of LottoNumber to Lotto constructor

--- src/test/kotlin/lotto/LottoGameTest.kt
+++ src/test/kotlin/lotto/LottoGameTest.kt
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.Test
 class LottoGameTest {
     @Test
     fun `should correctly count winning tickets`() {
-        val winningNumbers = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningNumbers = Lotto(listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) })
         val bonusNumber = 7
         val tickets =
             listOf(
-                Lotto(listOf(1, 2, 3, 4, 5, 6)),
-                Lotto(listOf(1, 2, 3, 4, 5, 7)),
-                Lotto(listOf(1, 2, 3, 4, 5, 45)),
-                Lotto(listOf(1, 2, 3, 4, 44, 45)),
-                Lotto(listOf(1, 2, 3, 43, 44, 45)),
-                Lotto(listOf(10, 11, 12, 13, 14, 15)),
+                Lotto(listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }),
+                Lotto(listOf(1, 2, 3, 4, 5, 7).map { LottoNumber.from(it) }),
+                Lotto(listOf(1, 2, 3, 4, 5, 45).map { LottoNumber.from(it) }),
+                Lotto(listOf(1, 2, 3, 4, 44, 45).map { LottoNumber.from(it) }),
+                Lotto(listOf(1, 2, 3, 43, 44, 45).map { LottoNumber.from(it) }),
+                Lotto(listOf(10, 11, 12, 13, 14, 15).map { LottoNumber.from(it) }),
             )
         val game = LottoGame()
         val result = game.play(tickets, winningNumbers, LottoNumber.from(bonusNumber))
@@ -29,20 +29,20 @@ class LottoGameTest {
 
     @Test
     fun `should correct count winning amount`() {
-        val winningNumbers = Lotto(listOf(1, 2, 3, 4, 5, 6))
+        val winningNumbers = Lotto(listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) })
         val bonusNumber = 7
         val tickets =
             listOf(
-                Lotto(listOf(1, 2, 3, 4, 5, 6)),
-                Lotto(listOf(1, 2, 3, 4, 5, 7)),
-                Lotto(listOf(1, 2, 3, 4, 5, 45)),
-                Lotto(listOf(1, 2, 3, 4, 44, 45)),
-                Lotto(listOf(1, 2, 3, 43, 44, 45)),
-                Lotto(listOf(10, 11, 12, 13, 14, 15)),
+                Lotto(listOf(1, 2, 3, 4, 5, 6).map { LottoNumber.from(it) }),
+                Lotto(listOf(1, 2, 3, 4, 5, 7).map { LottoNumber.from(it) }),
+                Lotto(listOf(1, 2, 3, 4, 5, 45).map { LottoNumber.from(it) }),
+                Lotto(listOf(1, 2, 3, 4, 44, 45).map { LottoNumber.from(it) }),
+                Lotto(listOf(1, 2, 3, 43, 44, 45).map { LottoNumber.from(it) }),
+                Lotto(listOf(10, 11, 12, 13, 14, 15).map { LottoNumber.from(it) }),
             )
         val game = LottoGame()
         val result = game.play(tickets, winningNumbers, LottoNumber.from(bonusNumber))
         val winningAmount = game.countWinningAmount(result)
-        assertEquals(winningAmount, 2_031_555_000)
+        assertEquals(winningAmount.value, 2_031_555_000)
     }
 }
