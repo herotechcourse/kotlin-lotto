415d17086729194a102d7b5a3055ea0e92bc1dec
Mina Kim <mina.kim@deliveryhero.com>
2025-07-14 16:08:33 +0200

    fix: package location in GameResultTest.kt

--- src/test/kotlin/lotto/domain/GameResultTest.kt
+++ src/test/kotlin/lotto/domain/GameResultTest.kt
@@ -1,7 +1,7 @@
 package lotto.domain
 
-import lotto.services.TicketIssuer
-import lotto.services.TicketsEvaluator
+import lotto.controller.TicketIssuer
+import lotto.controller.TicketsEvaluator
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
