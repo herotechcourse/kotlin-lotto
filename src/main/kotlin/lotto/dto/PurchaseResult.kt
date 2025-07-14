21deed9d8590e51e3c45b1b4eba94f1bfdd4bc0f
Mina Kim <mina.kim@deliveryhero.com>
2025-07-14 23:10:38 +0200

    chron: remove unused properties in PurchaseResult.kt
    - manualTickets, randomTickets

--- src/main/kotlin/lotto/dto/PurchaseResult.kt
+++ src/main/kotlin/lotto/dto/PurchaseResult.kt
@@ -5,7 +5,7 @@ import lotto.controller.TicketIssuer
 data class PurchaseResult(
     val userPurchase: UserPurchase,
 ) {
-    val manualTickets = TicketIssuer.issueManual(userPurchase.manualTicketsCount)
-    val randomTickets = TicketIssuer.issueRandom(userPurchase.randomTicketsCount)
-    val totalTickets = manualTickets + randomTickets
+    val totalTickets =
+        TicketIssuer.issueManual(userPurchase.manualTicketsCount) +
+            TicketIssuer.issueRandom(userPurchase.randomTicketsCount)
 }
