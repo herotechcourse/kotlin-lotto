e65a650a419bd8ebcb386b7d4c0dda0cb0220073
Mina Kim <mina.kim@deliveryhero.com>
2025-07-15 20:01:05 +0200

    refactor(lotto-number): change LottoNumber to value class

--- src/main/kotlin/lotto/domain/LottoNumber.kt
+++ src/main/kotlin/lotto/domain/LottoNumber.kt
@@ -1,6 +1,7 @@
 package lotto.domain
 
-data class LottoNumber(private val value: Int) {
+@JvmInline
+value class LottoNumber(private val value: Int) {
     companion object {
         internal const val MIN_RANGE_NUMBER = 1
         internal const val MAX_RANGE_NUMBER = 45
@@ -11,19 +12,5 @@ data class LottoNumber(private val value: Int) {
         }
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as LottoNumber
-        return value == other.value
-    }
-
-    override fun hashCode(): Int {
-        return value
-    }
-
-    override fun toString(): String {
-        return value.toString()
-    }
+    override fun toString(): String = value.toString()
 }
