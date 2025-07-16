4d5eaa6252a9b4bd44c5091002aceacb6d1fdba4
Mina Kim <mina.kim@deliveryhero.com>
2025-07-16 22:36:45 +0200

    test(number-generator): clean-up code

--- src/test/kotlin/lotto/domain/NumberGeneratorTest.kt
+++ src/test/kotlin/lotto/domain/NumberGeneratorTest.kt
@@ -3,7 +3,6 @@ package lotto.domain
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class NumberGeneratorTest {
     @Test
